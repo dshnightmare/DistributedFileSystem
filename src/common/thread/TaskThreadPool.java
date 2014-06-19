15a7592c89f427d9a3c50d88f1b29188f8a3484a
lishunyang <pkuuuuu@pku.edu.cn>
2014-06-19 11:29:53 +0800

    Finish the TaskThreadPool

--- src/common/thread/TaskThreadPool.java
+++ src/common/thread/TaskThreadPool.java
@@ -1,9 +1,55 @@
 package common.thread;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class TaskThreadPool
-{
-    private BlockingQueue<TaskThread> threads = new LinkedBlockingQueue<TaskThread>();
+public class TaskThreadPool {
+	private Queue<TaskThread> threads = new LinkedList<TaskThread>();
+
+	public synchronized void addThread(TaskThread thread) {
+		threads.add(thread);
+	}
+
+	public synchronized void removeThread(TaskThread thread) {
+		threads.remove(thread);
+	}
+
+	public synchronized TaskThread nextThread() {
+		TaskThread thread = threads.poll();
+		if (null != thread)
+			threads.add(thread);
+		return thread;
+	}
+
+	// Unit test
+	public static void main(String[] args) {
+		TaskThreadPool pool = new TaskThreadPool();
+		
+		pool.addThread(new TaskThread() {
+
+			@Override
+			public void run() {
+				System.out.println("TaskThread A");
+			}
+
+		});
+		pool.addThread(new TaskThread() {
+
+			@Override
+			public void run() {
+				System.out.println("TaskThread B");
+			}
+
+		});
+		
+		new Thread(pool.nextThread()).start();
+		new Thread(pool.nextThread()).start();
+		new Thread(pool.nextThread()).start();
+		new Thread(pool.nextThread()).start();
+		
+		TaskThread thread = pool.nextThread();
+		pool.removeThread(thread);
+		new Thread(pool.nextThread()).start();
+		new Thread(pool.nextThread()).start();
+	}
 }
