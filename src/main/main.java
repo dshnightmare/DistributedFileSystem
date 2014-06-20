ac01e1391c5a3937ab35dbb598fbe913327c772b
gengyf <gengyufeng0918@gmail.com>
2014-06-20 10:45:55 +0800

    main: choose startup type

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,38 @@
 package main;
 
+import java.util.Scanner;
+
+import common.network.ClientConnector;
+import common.network.ServerConnector;
+import common.observe.call.Call;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dsh sb");
+		Scanner input = new Scanner(System.in);
+		String startType = input.next();
+		
+		if(startType.equals("client")){
+			System.out.println("Start as client...");
+			ClientConnector client = new ClientConnector();
+			client.setupSocket();
+			while(true){
+				System.out.println("waiting for command(type(int) params[])");
+				Scanner in = new Scanner(System.in);
+				String cmd = in.next();
+				String param = in.nextLine();
+				String[] params = param.split(" ");
+				Call rc = new Call(Integer.parseInt(cmd), params);
+				client.sendCommand(rc);
+				System.out.println("Command:"+cmd+" "+param);
+			}
+		}
+		else if(startType.equals("Start as server...")){
+			System.out.println("dsh sb");
+			ServerConnector server = new ServerConnector();
+			server.setupSocket();
+		}
 	}
 
 }
