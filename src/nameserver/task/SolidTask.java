2e4b0f90e8ce6973a442604322a967c0cb8034d8
Lishunyang <lishunyang@pku.edu.cn>
2014-06-30 02:00:47 +0800

    Finish AddFileTask unit test.

--- src/nameserver/task/SolidTask.java
+++ src/nameserver/task/SolidTask.java
@@ -1,22 +1,86 @@
 package nameserver.task;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
+
 import nameserver.meta.Meta;
 import common.observe.call.Call;
 import common.thread.TaskThread;
+import common.util.Timestamp;
 
-public class SolidTask extends TaskThread
+public class SolidTask
+    extends TaskThread
 {
-    public SolidTask(long tid)
+    private String outputDirName;
+
+    private String fileName;
+
+    private long period;
+
+    public SolidTask(long tid, String outputDirName, String fileName,
+        long period)
     {
         super(tid);
+        this.outputDirName = outputDirName;
+        this.fileName = fileName;
+        this.period = period;
     }
 
     @Override
     public void handleCall(Call call)
     {
-        synchronized (Meta.getInstance())
+        File dir = new File(outputDirName);
+        if (!dir.exists())
+            dir.mkdirs();
+
+        while (true)
         {
-            // TODO
+            synchronized (Meta.getInstance())
+            {
+                String finalName =
+                    fileName + "-" + Timestamp.getInstance().getTimestamp();
+                Writer writer = null;
+                try
+                {
+                    writer = new FileWriter(finalName);
+                    JSONArray json = Meta.getInstance().getJsonData();
+                    writer.write(json.toString());
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    if (null != writer)
+                    {
+                        try
+                        {
+                            writer.close();
+                        }
+                        catch (IOException e)
+                        {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            try
+            {
+                TimeUnit.SECONDS.sleep(period);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 
@@ -25,7 +89,7 @@ public class SolidTask extends TaskThread
     {
         synchronized (Meta.getInstance())
         {
-            
+
         }
     }
 
--- src/test/nameserver/task/TestAddFileTask.java
+++ src/test/nameserver/task/TestAddFileTask.java
@@ -77,31 +77,35 @@ public class TestAddFileTask
         @Override
         public void handleCall(Call call)
         {
-            System.out.println("Server received a call: " + call.getType());
-            TaskThread task =
-                new AddFileTask(1, call, NConnector,
-                    Configuration.getInstance().getInteger(Configuration.DUPLICATE_KEY));
-            task.addListener(new TaskListener());
-            new Thread(task).start();
+            System.out.println("<---: " + call.getType());
+            if (Call.Type.ADD_FILE_C2N == call.getType())
+            {
+                TaskThread task =
+                    new AddFileTask(1, call, NConnector, Configuration
+                        .getInstance().getInteger(Configuration.DUPLICATE_KEY));
+                task.addListener(new TaskListener());
+                new Thread(task).start();
+            }
         }
     }
 
     private class CCallListener
         implements CallListener
     {
-
         @Override
         public void handleCall(Call call)
         {
-            System.out.println("Server sent a call: " + call.getType());
+            System.out.println("--->: " + call.getType());
             if (Call.Type.ADD_FILE_N2C == call.getType())
             {
                 AddFileCallN2C c = (AddFileCallN2C) call;
-                System.out.println(c.getTaskId());
-                System.out.println(c.getType());
-                System.out.println(c.getInitiator());
+                System.out.println("task id: " + c.getTaskId());
+                System.out.println("task type: " + c.getType());
+                System.out.println("initiator: " + c.getInitiator());
+                System.out.print("location: ");
                 for (String l : c.getLocations())
-                    System.out.println(l);
+                    System.out.print(l + " ");
+                System.out.println();
 
                 FinishCall ack = new FinishCall(call.getTaskId());
                 CConnector.sendCall(ack);
