83ac7d88e07a8e5ef0731e812b367cdf4fa1eddf
Lishunyang <lishunyang@pku.edu.cn>
2014-07-06 02:54:46 +0800

    Add file.

---
+++ src/nameserver/ui/MetaInfo.java
@@ -0,0 +1,74 @@
+package nameserver.ui;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+
+import client.ClientGUI;
+import nameserver.meta.Meta;
+
+public class MetaInfo
+{
+    private Meta meta;
+
+    private JPanel panel;
+
+    private DefaultListModel model;
+
+    private JList list;
+
+    public MetaInfo(Meta meta)
+    {
+        this.meta = meta;
+        panel = new JPanel();
+
+        // create directory
+        JButton button = new JButton("refresh");
+        button.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                updateList();
+            }
+        });
+
+        model = new DefaultListModel();
+        list = new JList(model);
+
+        panel.add(button, BorderLayout.EAST);
+        panel.add(list, BorderLayout.WEST);
+    }
+
+    public void updateList()
+    {
+        model.clear();
+        for (Entry<String, Map<String, String>> e : meta
+            .getValidDirectoryList().entrySet())
+        {
+            for (Entry<String, String> en : e.getValue().entrySet())
+            {
+                model.addElement(e.getKey() + " " + en.getKey() + " "
+                    + en.getValue());
+            }
+        }
+
+        panel.updateUI();
+    }
+
+    public JPanel getPanel()
+    {
+        return panel;
+    }
+}
