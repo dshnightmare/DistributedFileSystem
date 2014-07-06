5163613370967672c6e4dafdd840f527d7b7c4ac
dengsh <dshnightmare@gmail.com>
2014-07-06 16:55:21 +0800

    xxx

---
+++ src/storageserver/event/MigrateFileFinishEvent.java
@@ -0,0 +1,26 @@
+package storageserver.event;
+
+import java.util.List;
+
+import common.event.TaskEvent;
+import common.task.Task;
+
+public class MigrateFileFinishEvent extends TaskEvent {
+	private final String address;
+	private final List<String> files;
+
+	public MigrateFileFinishEvent(Task thread, String address, List<String> files) {
+		super(TaskEvent.Type.MIGRATE_FINISHED, thread);
+		this.address = address;
+		this.files = files;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public List<String> getFiles() {
+		return files;
+	}
+
+}
