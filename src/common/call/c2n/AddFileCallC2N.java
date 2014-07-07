64f0287242f85c619774a87c340601c735f162e8
Lishunyang <lishunyang@pku.edu.cn>
2014-07-07 21:24:32 +0800

    Add documentation of common.call.

--- src/common/call/c2n/AddFileCallC2N.java
+++ src/common/call/c2n/AddFileCallC2N.java
@@ -2,15 +2,36 @@ package common.call.c2n;
 
 import common.call.Call;
 
+/**
+ * Call of adding file, sent from <tt>Client</tt> to <tt>NameServer</tt>.
+ * 
+ * @author lishunyang
+ * 
+ */
 public class AddFileCallC2N
     extends Call
 {
+    /**
+     * Serial id.
+     */
     private static final long serialVersionUID = -805598603013291328L;
 
+    /**
+     * Directory name.
+     */
     private String dirName;
 
+    /**
+     * File name.
+     */
     private String fileName;
 
+    /**
+     * Construction method.
+     * 
+     * @param dirName
+     * @param fileName
+     */
     public AddFileCallC2N(String dirName, String fileName)
     {
         super(Call.Type.ADD_FILE_C2N);
@@ -18,11 +39,21 @@ public class AddFileCallC2N
         this.fileName = fileName;
     }
 
+    /**
+     * Get directory name.
+     * 
+     * @return
+     */
     public String getDirName()
     {
         return dirName;
     }
 
+    /**
+     * Get file name.
+     * 
+     * @return
+     */
     public String getFileName()
     {
         return fileName;
--- src/common/call/c2n/AppendFileCallC2N.java
+++ src/common/call/c2n/AppendFileCallC2N.java
@@ -2,15 +2,36 @@ package common.call.c2n;
 
 import common.call.Call;
 
+/**
+ * Call of appending file, sent from <tt>Client</tt> to <tt>NameServer</tt>.
+ * 
+ * @author lishunyang
+ * 
+ */
 public class AppendFileCallC2N
     extends Call
 {
+    /**
+     * Serial id.
+     */
     private static final long serialVersionUID = 5944057489040298495L;
 
+    /**
+     * Directory name.
+     */
     private final String dirName;
 
+    /**
+     * File name.
+     */
     private final String fileName;
 
+    /**
+     * Construction method.
+     * 
+     * @param dirName
+     * @param fileName
+     */
     public AppendFileCallC2N(String dirName, String fileName)
     {
         super(Call.Type.APPEND_FILE_C2N);
@@ -18,11 +39,21 @@ public class AppendFileCallC2N
         this.fileName = fileName;
     }
 
+    /**
+     * Get directory name.
+     * 
+     * @return
+     */
     public String getDirName()
     {
         return dirName;
     }
 
+    /**
+     * Get file name.
+     * 
+     * @return
+     */
     public String getFileName()
     {
         return fileName;
