001cf5e1654d4e2b088601261049dd9b2acb04cd
Lishunyang <lishunyang@pku.edu.cn>
2014-07-07 23:04:07 +0800

    Add documentation of common.task.

--- src/common/event/TaskEventDispatcher.java
+++ src/common/event/TaskEventDispatcher.java
@@ -1,10 +1,32 @@
 package common.event;
 
+/**
+ * The <tt>TaskEvent</tt> dispatcher.
+ * 
+ * @author lishunyang
+ * @see TaskEvent
+ * @see TaskEventListener
+ */
 public interface TaskEventDispatcher
 {
+    /**
+     * 
+     * 
+     * @param listener
+     */
     public void addListener(TaskEventListener listener);
 
+    /**
+     * Remove <tt>TaskEventListener</tt>.
+     * 
+     * @param listener
+     */
     public void removeListener(TaskEventListener listener);
 
+    /**
+     * Fire <tt>TaskEvent</tt>.
+     * 
+     * @param event
+     */
     public void fireEvent(TaskEvent event);
 }
--- src/common/event/TaskEventListener.java
+++ src/common/event/TaskEventListener.java
@@ -1,6 +1,17 @@
 package common.event;
 
+/**
+ * Listener of <tt>TaskEvent</tt>.
+ * 
+ * @author lishunyang
+ * @see TaskEvent
+ */
 public interface TaskEventListener
 {
+    /**
+     * Handle <tt>TaskEvent</tt>.
+     * 
+     * @param event
+     */
     public void handle(TaskEvent event);
 }
